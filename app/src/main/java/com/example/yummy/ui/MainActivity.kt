9235db943d2b7ab94eff0e5d6ef399783b90a1d8
fedichebbi <35900204+fedichebbi@users.noreply.github.com>
2020-01-02 11:49:42 +0100

    first commit

---
+++ app/src/main/java/com/example/yummy/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.yummy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yummy.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
